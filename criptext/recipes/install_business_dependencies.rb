9cef85223f7ca72f0c49d8fc80d58824be8988c5
Daniel Tigse <danieltp15@hotmail.com>
2020-07-05 22:58:48 -0500

    node 10 again

--- criptext/recipes/install_business_dependencies.rb
+++ criptext/recipes/install_business_dependencies.rb
@@ -7,13 +7,16 @@ bash 'install node from RPM' do
 end
 
 remote_file '/root/nvm_install.sh' do
-  source 'https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh'
+  source 'https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh'
 end
 
 bash 'install node via nvm' do
   code <<-EOH
     bash '/root/nvm_install.sh'
     source '/root/.nvm/nvm.sh'
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvm install 10
   EOH
 end
